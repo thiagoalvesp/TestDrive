731023b9eab23be234c37550a3b384a67333ec3a
Thiago Alves Pereira <thiago-alvesp@live.com>
2018-10-14 22:36:09 -0300

    Fim modulo 1

--- TestDrive/TestDrive/MainPage.xaml.cs
+++ TestDrive/TestDrive/MainPage.xaml.cs
@@ -7,18 +7,30 @@ using Xamarin.Forms;
 
 namespace TestDrive
 {
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+
     public partial class MainPage : ContentPage
     {
+        public List<Veiculo> Veiculos { get; set; }
         public MainPage()
         {
             InitializeComponent();
 
-            ListViewVeiculos.ItemsSource = new string[] 
+            Veiculos = new List<Veiculo>
             {
-                "Azera V6",
-                "Fista 2.0",
-                "HB20 S"
+                new Veiculo{Nome= "Azera V6",Preco = 60000},
+                new Veiculo{Nome= "Fiesta 2.0",Preco = 50000},
+                new Veiculo{Nome= "HB20 S",Preco = 40000}
             };
+
+            //ListViewVeiculos.ItemsSource = Veiculos;
+
+            this.BindingContext = this;
+
         }
     }
 }
